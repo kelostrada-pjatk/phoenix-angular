c646a07e22e0067dcfc6156abb827ab5ebb2b1e6
kelostrada <kelostrada@gmail.com>
2016-01-30 13:09:20 +0100

    js changes

--- web/static/js/controllers.js
+++ web/static/js/controllers.js
@@ -1,1 +1,29 @@
-﻿var computersApp = angular.module("computersApp",["ngRoute"]);
+﻿
+var computersApp = angular.module("computersApp",["ngRoute"]);
+
+computersApp.config(function ($routeProvider){
+    $routeProvider.
+    when("/computers",{
+        templateUrl:"./computers",
+        controller:"computersCtrl"
+    }).
+    when("/computer/:id",{
+        templateUrl:"./computers/show",
+        controller:"computerCtrl"
+    })
+    .otherwise({
+        redirectTo:"/computers"
+    });
+});
+
+computersApp.controller("computersCtrl", function() {
+
+});
+
+computersApp.controller("computerCtrl", function() {
+
+});
+
+var test = function() {
+    console.log("test");
+}
